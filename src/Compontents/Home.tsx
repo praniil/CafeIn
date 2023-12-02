1e213b9d37df1ceb222d21093daf627f7f7217cc
praniil <pparajuli15@gmail.com>
2023-12-02 22:25:38 +0545

    feat: added routing props in the slider

--- src/Compontents/Home.tsx
+++ src/Compontents/Home.tsx
@@ -1,26 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import coffeeTypes from "../typescoffee.png";
 import tableCoffee from "../coffeeTable.jpg";
 import americano from "../coffee/americano.jpg";
 import arabiccoffee from "../coffee/arabic-coffee.jpg";
 import cappuccino from "../coffee/cappuccino2.jpg";
+import latte from "../coffee/latte.jpg";
 import cortado from "../coffee/Cortado.jpg";
 import espresso from "../coffee/espresso.jpg";
 import frappe from "../coffee/Frappe.jpg";
-import latte from "../coffee/latte.jpg";
 import macchiator from "../coffee/macchiato.jpg";
 import mocha from "../coffee/mocha.jpg";
 import ristretto from "../coffee/ristretto.jpg";
 import turkish from "../coffee/turkish.jpg";
 
-const photoSliderArray = [
-  {
-    url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstocksnap.io%2Fsearch%2Fcoffee&psig=AOvVaw33oUYKy01JbvkoNMYQZdLQ&ust=1701618730573000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCKjk3PGN8YIDFQAAAAAdAAAAABAJ",
-  },
-  {
-    url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstocksnap.io%2Fsearch%2Fcoffee&psig=AOvVaw33oUYKy01JbvkoNMYQZdLQ&ust=1701618730573000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCKjk3PGN8YIDFQAAAAAdAAAAABAJ",
-  },
-];
+const photoSliderArray: string[] = [americano, arabiccoffee, cappuccino, latte];
 
 const Home = () => {
   return (
@@ -29,12 +23,23 @@ const Home = () => {
         <img src={tableCoffee} alt="" />
       </div>
       <div>
-        {photoSliderArray.map((photo, index) => (
-          <div>
-            {" "}
-            <img key={index} src={photo.url} alt="" />
-          </div>
-        ))}
+        <p className="text-center mt-2 font-bold font-mono text-red-800">
+          {" "}
+          OUR SPECIAL COFFEE
+        </p>
+        <Link to={`/menu/${photoSliderArray[0]}`}>
+          <img
+            className="w-96 h-auto m-auto pt-2 pb-5"
+            src={photoSliderArray[0]}
+            alt=""
+          />
+        </Link>
+        <button className="absolute top-1/2 left-2 transform -translate-y-1/2">
+          L
+        </button>
+        <button className="absolute top-1/2 right-2 transform -translate-y-1/2">
+          R
+        </button>
       </div>
     </div>
   );
