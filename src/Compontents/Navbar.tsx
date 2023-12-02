38fc742504957523c1ea1c066c9d8e2b59920f92
praniil <pparajuli15@gmail.com>
2023-12-02 16:00:51 +0545

    feat: navbar completed

--- src/Compontents/Navbar.tsx
+++ src/Compontents/Navbar.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
+const navbarElements: string[] = ["Menu", "Cart", "Book A Table"];
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="bg-red-950 text-white">
+      <div className="container mx-auto">
+        <ul className="flex justify-between items-center h-16">
+          <li>
+            <Link to="/" className="text-2xl font-bold pl-6">
+              CafeIn
+            </Link>
+          </li>
+          <div className="flex items-center space-x-8">
+            {navbarElements.map((element, index) => (
+              <li key={index} className="mx-3">
+                <Link
+                  to={`/${element.toLowerCase().replaceAll(" ", "-")}`}
+                  className="hover:text-gray-300"
+                >
+                  {element}
+                </Link>
+              </li>
+            ))}
+          </div>
+          <div className="flex items-center space-x-6 pr-4">
+            <li>
+              <Link to="/sign-in" className="text-lg font-medium ">
+                Sign In
+              </Link>
+            </li>
+            <li>
+              <Link to="/join-now" className="text-lg font-medium">
+                Join Now
+              </Link>
+            </li>
+          </div>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
